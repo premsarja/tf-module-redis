4fab3aa5af0a61b973ba4e54e6d891dc19aa316a
premsarja <premsagar@prodot.in>
2024-03-25 17:59:10 +0530

    done

--- data.tf
+++ data.tf
@@ -8,3 +8,14 @@ data "terraform_remote_state" "vpc" {
     }
 }
 
+# fetches the secret informatiuon from secret
+
+data "aws_secretsmanager_secret" "secret" {
+  name = "robot/secrets"  
+}
+
+# fetches the info from the above secret
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
+}
