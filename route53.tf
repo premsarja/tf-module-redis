751389d0adb563cb453faad4e503806f67ad187e
premsarja <premsagar@prodot.in>
2024-03-25 14:37:47 +0530

    done

---
+++ route53.tf
@@ -0,0 +1,9 @@
+# creates record internal aws_route53_record
+
+ resource "aws_route53_record" "redis" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_elasticache_cluster.redis.cluster_address]
+}   
